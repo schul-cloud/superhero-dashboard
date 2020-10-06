abc48404080cf104e82b9505f3bb943b463fad63
Michael Wolowyk <michael.wolowyk@capgemini.com>
2020-10-06 16:45:11 +0200

    remove duplicates from the timezones and improved sorting

--- controllers/schools.js
+++ controllers/schools.js
@@ -11,30 +11,36 @@ const moment = require('moment-timezone');
 
 moment.locale('de');
 
-let countryTimezones = moment.tz.countries();
-countryTimezones = countryTimezones.map((item) => {
+const removeDuplicates = (myArr, prop) => {
+    return myArr.filter((obj, pos, arr) => {
+        return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
+};
+
+const calculateOffsetHours = (offset) => {
+    let prefix = (offset >= 0) ? '+' : '-';
+    const hours = String(Math.floor(Math.abs(offset)/60)).padStart(2, '0');
+    const minutes = String(Math.abs(offset) % 60).padStart(2, '0');
+    return hours === '00' ? '(UTC)' : `(UTC${prefix}${hours}:${minutes})`;
+};
+
+const countries = moment.tz.countries();
+let countryTimezones = countries.map((item) => {
     return moment.tz.zonesForCountry(item, true);
 });
+
 countryTimezones = [].concat.apply([], countryTimezones);
 countryTimezones = countryTimezones.map((item) => {
     return {
         ...item,
-        offset: item.offset * -1
+        offsetMin: item.offset * -1,
+        offset: calculateOffsetHours(item.offset * -1)
     };
 });
 
-countryTimezones.sort((a, b) => b.offset - a.offset);
-countryTimezones = countryTimezones.map((item) => {
-    const {offset} = item;
-    let prefix = (offset >= 0) ? '+' : '-';
-    const hours = String(Math.floor(Math.abs(offset)/60)).padStart(2, '0');
-    const minutes = String(Math.abs(offset) % 60).padStart(2, '0');
+countryTimezones = removeDuplicates(countryTimezones, 'name');
+countryTimezones.sort((a, b) => (b.offsetMin - a.offsetMin) + (b.name >= a.name ? 1 : -1) );
 
-    return {
-        ...item,
-        offset: hours === '00' ? '(UTC)' : `(UTC ${prefix}${hours}:${minutes})`
-    };
-});
 countryTimezones = countryTimezones.reverse();
 
 const SCHOOL_FEATURES = [
